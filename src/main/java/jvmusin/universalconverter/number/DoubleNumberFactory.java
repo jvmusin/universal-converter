9166016dfcb1f8f0130180c7f0781a763e65f2c0
Rustam Musin <jvmusin@gmail.com>
2021-03-15 20:50:00 +0400

    Clarify javadoc in DoubleNumberFactory

--- src/main/java/jvmusin/universalconverter/number/DoubleNumberFactory.java
+++ src/main/java/jvmusin/universalconverter/number/DoubleNumberFactory.java
@@ -1,16 +1,25 @@
 package jvmusin.universalconverter.number;
 
-import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
 /** Фабрика, используемая для создания чисел типа {@link DoubleNumber}. */
-@Component
 public class DoubleNumberFactory implements NumberFactory<DoubleNumber> {
   @Override
   public DoubleNumber one() {
     return new DoubleNumber(1);
   }
 
+  /**
+   * Парсит строку в число.
+   *
+   * <p>Парсинг производится методом {@link Double#parseDouble(String)}.
+   *
+   * @param s строка для парсинга в число.
+   * @return Спарсенное число.
+   * @throws IllegalArgumentException если строка равна {@code null}.
+   * @throws NumberFormatException если число спарсить не удалось.
+   * @see Double#parseDouble(String)
+   */
   @Override
   public DoubleNumber parse(String s) {
     Assert.notNull(s, "Строка не может быть null");
